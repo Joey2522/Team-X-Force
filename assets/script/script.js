101c30cba359930431be65bda535eeedf79ecff9
Joey2522 <148152563+Joey2522@users.noreply.github.com>
2023-12-10 15:33:10 -0600

    dropwdown menu working

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,85 +1,101 @@
-let recipeImgEl = document.getElementById('recipe-img');
-let recipeNameEl = document.getElementById('recipe-name');
-let randomButton = document.getElementById('random-btn');
-let recipeButton = document.getElementById('generate-btn');
-let flagButton = document.getElementById('generate-btn');
+// let recipeImgEl = document.getElementById('recipe-img');
+// let recipeNameEl = document.getElementById('recipe-name');
+// let randomButton = document.getElementById('random-btn');
+// let recipeButton = document.getElementById('get-recipe-btn');
+// let flagButton = document.getElementById('generate-btn');
+// let optionsArray = ["United States", "Egypt", "Italy", "Spain", "Britain", "Brazil", "Russia", "China", "Thailand", "Jamaica"];
+// let dropDown = document.getElementById('country-input');
 
-function getRandom() {
-    let requestUrl = "https://www.themealdb.com/api/json/v1/1/filter.php?a=Canadian";
+//     for (var i = 0; i < optionsArray.length; i++)
+//         var option = document.createElement("option");
+//         option.value = optionsArray[i];
+//         option.text = optionsArray[i];
+//         dropDown.appendChild(option);
 
-    fetch(requestUrl)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-            for (var i = 0; i < data.meals.length; i++) 
-            var randomIndex = Math.floor(Math.random()*data.meals.length);
-            var randomMeal = data.meals[randomIndex];
-                let recipeName = document.createElement('h2');
-                let recipeImg = document.createElement('p');
-                recipeName.textContent = randomMeal.strMeal;
-                recipeImg.textContent = randomMeal.strMealThumb;
-                recipeNameEl.append(recipeName);
-                recipeImgEl.append(recipeImg);
-            }).catch(error => {
-                console.log(error);
-    })
-};
 
-function getRecipe() {
-    let requestUrl = "https://www.themealdb.com/api/json/v1/1/filter.php?a=Canadian";
+// function getRandom() {
+//     let requestUrl = "https://www.themealdb.com/api/json/v1/1/filter.php?a=Canadian";
+
+//     fetch(requestUrl)
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (data) {
+//         console.log(data);
+//             for (var i = 0; i < data.meals.length; i++) 
+//             var randomIndex = Math.floor(Math.random()*data.meals.length);
+//             var randomMeal = data.meals[randomIndex];
+//                 let recipeName = document.createElement('h2');
+//                 let recipeImg = document.createElement('p');
+//                 recipeName.textContent = randomMeal.strMeal;
+//                 recipeImg.textContent = randomMeal.strMealThumb;
+//                 recipeNameEl.append(recipeName);
+//                 recipeImgEl.append(recipeImg);
+//             }).catch(error => {
+//                 console.log(error);
+//     })
+// };
+
+// function getRecipe() {
+//     let requestUrl = "https://www.themealdb.com/api/json/v1/1/filter.php?a=Canadian";
     
-    fetch(requestUrl)
-    .then(function (response) {
-         return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-             for (var i = 0; i < data.meals.length; i++) 
-            var randomIndex = Math.floor(Math.random()*data.meals.length)
-            var randomMeal = data.meals[randomIndex];
-                console.log("here")
-                let recipeName = document.createElement('h2');
-                let recipeImg = document.createElement('p');
-                recipeName.textContent = randomMeal.strMeal;
-                recipeImg.textContent = randomMeal.strMealThumb;
-                recipeNameEl.append(recipeName);
-                recipeImgEl.append(recipeImg);
-            }).catch(error => {
-                console.log(error);
-    })
-};
+//     fetch(requestUrl)
+//     .then(function (response) {
+//          return response.json();
+//     })
+//     .then(function (data) {
+//         console.log(data);
+//              for (var i = 0; i < data.meals.length; i++) 
+//             var randomIndex = Math.floor(Math.random()*data.meals.length)
+//             var randomMeal = data.meals[randomIndex];
+//                 console.log("here")
+//                 let recipeName = document.createElement('h2');
+//                 let recipeImg = document.createElement('p');
+//                 recipeName.textContent = randomMeal.strMeal;
+//                 recipeImg.textContent = randomMeal.strMealThumb;
+//                 recipeNameEl.append(recipeName);
+//                 recipeImgEl.append(recipeImg);
+//             }).catch(error => {
+//                 console.log(error);
+//     })
+// };
 
-function getFlag() {
-    let requestUrl = "https://restcountries.com/v3.1/all";
+// function getFlag() {
+//     let requestUrl = "https://restcountries.com/v3.1/all";
     
-    fetch(requestUrl)
-    .then(function (response) {
-         return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-             for (var i = 0; i < data.meals.length; i++) 
-            // var randomIndex = Math.floor(Math.random()*data.meals.length)
-            // var randomMeal = data.meals[randomIndex];
-                console.log(data)
-                let recipeName = document.createElement('h2');
-                let recipeImg = document.createElement('p');
-                recipeName.textContent = randomMeal.strMeal;
-                recipeImg.textContent = randomMeal.strMealThumb;
-                recipeNameEl.append(recipeName);
-                recipeImgEl.append(recipeImg);
-            }).catch(error => {
-                console.log(error);
-    })
+//     fetch(requestUrl)
+//     .then(function (response) {
+//          return response.json();
+//     })
+//     .then(function (data) {
+//         console.log(data);
+//              for (var i = 0; i < data.meals.length; i++) 
+//             // var randomIndex = Math.floor(Math.random()*data.meals.length)
+//             // var randomMeal = data.meals[randomIndex];
+//                 console.log(data)
+//                 let recipeName = document.createElement('h2');
+//                 let recipeImg = document.createElement('p');
+//                 recipeName.textContent = randomMeal.strMeal;
+//                 recipeImg.textContent = randomMeal.strMealThumb;
+//                 recipeNameEl.append(recipeName);
+//                 recipeImgEl.append(recipeImg);
+//             }).catch(error => {
+//                 console.log(error);
+//     })
+// };
+
+// randomButton.addEventListener('click', getRandom);
+// recipeButton.addEventListener('click', getRecipe);
+// recipeButton.addEventListener('click', getDrop);
+// flagButton.addEventListener('click', getFlag);
+
+let dropDown = document.querySelector('.dropDown');
+dropDown.onclick = function(){
+    dropDown.classList.toggle('active');
 };
 
-randomButton.addEventListener('click', getRandom);
-recipeButton.addEventListener('click', getRecipe);
-flagButton.addEventListener('click', getFlag);
-
-
-
+function show(anything) {
+    document.querySelector('.countryInput').value = anything;
+}
 
 
