76eb96baeaab435471db3672eeb56777cf517055
Joey2522 <148152563+Joey2522@users.noreply.github.com>
2023-12-11 21:35:04 -0600

    more local storage code

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,7 +1,8 @@
 let recipeImgEl = document.getElementById('recipe-img');
 let recipeNameEl = document.getElementById('recipe-name');
-let randomButton = document.getElementById('random-btn');
+// let randomButton = document.getElementById('random-btn');
 let flagButton = document.getElementById('generate-btn');
+let favButton = document.getElementById('favorite-btn');
 
 // let dropDown = document.getElementById('country-input');
 
@@ -12,28 +13,28 @@ let flagButton = document.getElementById('generate-btn');
     //     dropDown.appendChild(option);
 
 
-function getRandom() {
-    let requestUrl = "https://www.themealdb.com/api/json/v1/1/filter.php?a=Canadian";
+// function getRandom() {
+//     let requestUrl = "https://www.themealdb.com/api/json/v1/1/filter.php?a=Canadian";
 
-    fetch(requestUrl)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-            for (var i = 0; i < data.meals.length; i++) 
-            var randomIndex = Math.floor(Math.random()*data.meals.length);
-            var randomMeal = data.meals[randomIndex];
-                let recipeName = document.createElement('h2');
-                let recipeImg = document.createElement('p');
-                recipeName.textContent = randomMeal.strMeal;
-                recipeImg.textContent = randomMeal.strMealThumb;
-                recipeNameEl.append(recipeName);
-                recipeImgEl.append(recipeImg);
-            }).catch(error => {
-                console.log(error);
-    })
-};
+//     fetch(requestUrl)
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (data) {
+//         console.log(data);
+//             for (var i = 0; i < data.meals.length; i++) 
+//             var randomIndex = Math.floor(Math.random()*data.meals.length);
+//             var randomMeal = data.meals[randomIndex];
+//                 let recipeName = document.createElement('h2');
+//                 let recipeImg = document.createElement('p');
+//                 recipeName.textContent = randomMeal.strMeal;
+//                 recipeImg.textContent = randomMeal.strMealThumb;
+//                 recipeNameEl.append(recipeName);
+//                 recipeImgEl.append(recipeImg);
+//             }).catch(error => {
+//                 console.log(error);
+//     })
+// };
 
 function getRecipe(country) {
     let requestUrl = "https://www.themealdb.com/api/json/v1/1/filter.php?a=" + country;
@@ -47,7 +48,6 @@ function getRecipe(country) {
              for (var i = 0; i < data.meals.length; i++) 
             var randomIndex = Math.floor(Math.random()*data.meals.length)
             var randomMeal = data.meals[randomIndex];
-                console.log("here")
                 let recipeName = document.createElement('h2');
                 let recipeImg = document.createElement('p');
                 recipeName.textContent = randomMeal.strMeal;
@@ -83,13 +83,11 @@ function getFlag() {
     })
 };
 
-randomButton.addEventListener('click', getRandom);
-// recipeButton.addEventListener('click', getRecipe);
-// recipeButton.addEventListener('click', getDrop);
+// randomButton.addEventListener('click', getRandom);
 // flagButton.addEventListener('click', getFlag);
 
 let dropDown = document.querySelector('.dropDown');
-dropDown.onclick = function(){
+dropDown.onclick = function() {
     dropDown.classList.toggle('active');
 };
 
@@ -98,5 +96,26 @@ function show(anything) {
     getRecipe(anything);
     console.log(anything);
 }
+    //saves the entered text to the local storage when the save button is hit
+// function saveRecipe() {
+//     $('favorite-btn').on('click', function() {
+//         const key = $(this).parent().attr('id');
+//         const value = $(this).siblings('.description').val();
+//         localStorage.setItem(key, value);
+//         console.log(favorite);
+//     });
+// }
 
+function saveRecipe() {
+    var chosenRecipe = document.querySelector();
+    localStorage.setItem('myData', JSON.stringify(chosenRecipe));
+}
+    
+      //allows the saved text to remain in the local storage even after a page refresh
+    $('.time-block').each(function() {
+        const key = $(this).attr('id');
+        const value = localStorage.getItem(key);
+        $(this).children('.description').val(value);
+});
 
+// saveRecipe();
